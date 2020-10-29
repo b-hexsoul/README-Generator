3c3b55047b48ce13c1912fc8de968544e5a4bdb6
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2020-10-29 01:11:22 -0700

    'Updated generateMarkdown and uploading test readme'

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 
-    # Brandons REpo
+    [![MIT License]()](https://opensource.org/licenses/MIT)
+    # Brandon's New Project
 
     ## Description
-    New repo baby
+    This is a repo to describe this project because it is awesome. This project creates an awesome readme file. Next Ill be putting all my focus into OOP with Javascript. This is fundamental for my growth and learning and to future projects I take on. I need to dive deep into this and give it my all.
 
     ## Table of Contents
     * [Installation](#installation)
@@ -11,16 +12,23 @@
     * [Contributing](#contributing)
     * [Tests](#tests)
     * [Questions](#questions)
+    
     ## Installation
-    No
+    There are no steps for installation
+    
     ## Usage
     Instructions coming soon
+    
     ## License
     MIT License
+    This application is covered under MIT License, visit https://opensource.org/licenses/MIT for more information
+    
     ## Contributing
     false
+    
     ## Tests
     No tests
+    
     ## Questions
-    Nope
+    No questions
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let badge = '';
+  let licenseText = '';
+  
+  if (data.license === 'MIT License') {
+    badge = '[![MIT License]()](https://opensource.org/licenses/MIT)'
+    licenseText = 'This application is covered under MIT License, visit https://opensource.org/licenses/MIT for more information'
+  } else if (data.license === 'GNU GPLv3') {
+    badge = '[![GNU GPLv3]()](https://www.gnu.org/licenses/gpl-3.0.en.html)'
+    licenseText = 'This application is covered under GNU GPLv3, visit https://www.gnu.org/licenses/gpl-3.0.en.html for more information'
+  } else if (data.license === 'ISC License') {
+    badge = '[![ISC License]()](https://opensource.org/licenses/ISC)'
+    licenseText = 'This application is covered under ISC License, visit https://opensource.org/licenses/ISC for more information'
+  } else if (data.license === 'Apache License 2.0') {
+    badge = '[![Apache 2.0]()](https://www.apache.org/licenses/LICENSE-2.0)'
+    licenseText = 'This application is covered under Apache License 2.0, visit https://www.apache.org/licenses/LICENSE-2.0 for more information'
+  } else if (data.license === 'Mozilla Public License 2.0') {
+    badge = '[![Mozilla License 2.0]()](https://www.mozilla.org/en-US/MPL/2.0/)'
+    licenseText = 'This application is covered under Mozilla License 2.0, visit https://www.mozilla.org/en-US/MPL/2.0/ for more information'
+  }
+
+
+  //"GNU GPLv3", "ISC License", "Apache License 2.0", "Mozilla Public License 2.0"
   return `
+    ${badge}
     # ${data.title}
 
     ## Description
@@ -22,6 +45,7 @@ function generateMarkdown(data) {
     
     ## License
     ${data.license}
+    ${licenseText}
     
     ## Contributing
     ${data.contributing}
